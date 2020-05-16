1b24133e9e457ef58550a65f6ceff7fa0fca6e6b
Bruno Silva <brunoalexmsilva@gmail.com>
2020-05-16 01:10:58 +0100

    UdpSrv a funcionar da mesma forma que o UdpSrv.java

--- UdpSrv.py
+++ UdpSrv.py
@@ -1,26 +1,22 @@
 import socket
 import sys
 
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 data = 300
 
 msgFromServer       = "Hello UDP Client"
 bytesToSend         = str.encode(msgFromServer)
  
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# Bind to address and ip
 try:
-    UDPServerSocket.bind(('', 9999))
+    sock.bind(('', 9999))
 except socket.error as msg:
 	print("Bind to local port failed")
 	sys.exit()
 
 print("Listening for UDP requests (IPv6/IPv4). Use CTRL+C to terminate the server") 
-
-# Listen for incoming datagrams
 while(True):
-    udpPacket = UDPServerSocket.recvfrom(data)
-    address = udpPacket[1]
-    print("Request from: {} port: {}".format(address[0]), address[1])
-    UDPServerSocket.sendto(bytesToSend, address)
+    udpPacket = sock.recvfrom(data)
+    print("Request from: {} port: {}".format(udpPacket[1][0], udpPacket[1][1]))
+    data1 = udpPacket[0].decode() [::-1]
+    sock.sendto(str.encode(data1), udpPacket[1])
